24cb414fcd246f8de1aa5aa6ce19865391db6d21
Jan Pikl <pikl.jan@gmail.com>
2020-03-29 23:42:06 +0200

    Define pattern module types

--- src/pattern/types.rs
+++ src/pattern/types.rs
@@ -1,4 +1,18 @@
 #[derive(Debug, PartialEq)]
+pub struct Pattern {
+    items: Vec<Parsed<PatternItem>>,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum PatternItem {
+    Constant(String),
+    Expression {
+        variable: Parsed<Variable>,
+        transforms: Vec<Parsed<Transform>>,
+    },
+}
+
+#[derive(Debug, PartialEq)]
 pub enum Variable {
     Filename,
     Basename,
@@ -8,4 +22,32 @@ pub enum Variable {
     GlobalCounter,
     CaptureGroup(usize),
     Uuid,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Transform {
+    Character(usize),
+    SubstrFrom(usize),
+    SubstrTo(usize),
+    SubstrFromTo(usize, usize),
+    EndCharacter(usize),
+    EndSubstrFrom(usize),
+    EndSubstrTo(usize),
+    EndSubstrFromTo(usize, usize),
+    ReplaceFirst(String, String),
+    ReplaceAll(String, String),
+    Trim,
+    Lowercase,
+    Uppercase,
+    ToAscii,
+    RemoveNonAscii,
+    LeftPad(String),
+    RightPad(String),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Parsed<T> {
+    value: T,
+    source_position: usize,
+    source_length: usize,
 }
