1eac0f9fce52b4ecff37463b4b9c81a924bc4bcd
Jan Pikl <pikl.jan@gmail.com>
2020-11-05 11:50:28 +0100

    Finish common::transfer::fs mod + add tests

--- src/common/transfer/fs.rs
+++ src/common/transfer/fs.rs
@@ -2,12 +2,13 @@ use crate::fs::FileType;
 use fs_extra::error::{Error, ErrorKind, Result};
 use fs_extra::{dir, file};
 use lazy_static::lazy_static;
+use std::fs;
 use std::path::Path;
 
 #[derive(Clone, Copy)]
 pub enum TransferMode {
-    Copy,
     Move,
+    Copy,
 }
 
 pub fn transfer_path(src_path: &Path, dst_path: &Path, mode: TransferMode) -> Result<()> {
@@ -39,32 +40,32 @@ pub fn transfer_path(src_path: &Path, dst_path: &Path, mode: TransferMode) -> Re
         )),
 
         (FileType::File, _) => {
-            // TODO test
             if let Some(dst_parent) = dst_path.parent() {
                 dir::create_all(dst_parent, false)?;
             }
             match mode {
+                TransferMode::Move => {
+                    if fs::rename(src_path, dst_path).is_err() {
+                        file::move_file(src_path, dst_path, &FILE_COPY_OPTIONS)?;
+                    }
+                }
                 TransferMode::Copy => {
                     file::copy(src_path, dst_path, &FILE_COPY_OPTIONS)?;
-                }
-                TransferMode::Move => {
-                    // TODO try rename first
-                    file::move_file(src_path, dst_path, &FILE_COPY_OPTIONS)?;
                 }
             }
             Ok(())
         }
 
         (FileType::Dir, _) => {
-            // TODO test
             dir::create_all(dst_path, false)?;
             match mode {
+                TransferMode::Move => {
+                    if fs::rename(src_path, dst_path).is_err() {
+                        dir::move_dir(src_path, dst_path, &DIR_COPY_OPTIONS)?;
+                    }
+                }
                 TransferMode::Copy => {
                     dir::copy(src_path, dst_path, &DIR_COPY_OPTIONS)?;
-                }
-                TransferMode::Move => {
-                    // TODO try rename first
-                    dir::move_dir(src_path, dst_path, &DIR_COPY_OPTIONS)?;
                 }
             }
             Ok(())
@@ -113,7 +114,7 @@ mod tests {
         let src_file = NamedTempFile::new("a").unwrap();
 
         assert_eq!(
-            transfer_path(src_file.path(), &Path::new("b"), TransferMode::Copy)
+            transfer_path(src_file.path(), &Path::new("b"), TransferMode::Move) // Mode is irrelevant
                 .map_err(unpack_fse_error),
             Err((
                 debug_fse_error_kind(ErrorKind::NotFound),
@@ -130,11 +131,12 @@ mod tests {
     #[test]
     fn overwrite_dir_with_file_error() {
         let src_file = NamedTempFile::new("a").unwrap();
-        src_file.touch().unwrap();
         let dst_dir = TempDir::new().unwrap();
 
+        src_file.touch().unwrap();
+
         assert_eq!(
-            transfer_path(src_file.path(), dst_dir.path(), TransferMode::Copy)
+            transfer_path(src_file.path(), dst_dir.path(), TransferMode::Move) // Mode is irrelevant
                 .map_err(unpack_fse_error),
             Err((
                 debug_fse_error_kind(ErrorKind::Other),
@@ -154,10 +156,11 @@ mod tests {
     fn overwrite_file_with_dir_error() {
         let src_dir = TempDir::new().unwrap();
         let dst_file = NamedTempFile::new("a").unwrap();
+
         dst_file.touch().unwrap();
 
         assert_eq!(
-            transfer_path(src_dir.path(), dst_file.path(), TransferMode::Copy)
+            transfer_path(src_dir.path(), dst_file.path(), TransferMode::Move) // Mode is irrelevant
                 .map_err(unpack_fse_error),
             Err((
                 debug_fse_error_kind(ErrorKind::Other),
@@ -171,5 +174,185 @@ mod tests {
 
         src_dir.assert(predicates::path::is_dir());
         dst_file.assert(predicates::path::is_file());
+    }
+
+    #[test]
+    fn rename_file() {
+        let src_file = NamedTempFile::new("a").unwrap();
+        let dst_file = NamedTempFile::new("b").unwrap();
+
+        src_file.write_str("1").unwrap();
+
+        assert_eq!(
+            transfer_path(src_file.path(), dst_file.path(), TransferMode::Move)
+                .map_err(unpack_fse_error),
+            Ok(())
+        );
+
+        src_file.assert(predicates::path::missing());
+        dst_file.assert("1");
+    }
+
+    #[test]
+    fn move_overwrite_file() {
+        let src_file = NamedTempFile::new("a").unwrap();
+        let dst_file = NamedTempFile::new("b").unwrap();
+
+        src_file.write_str("1").unwrap();
+        dst_file.touch().unwrap();
+
+        assert_eq!(
+            transfer_path(src_file.path(), dst_file.path(), TransferMode::Move)
+                .map_err(unpack_fse_error),
+            Ok(())
+        );
+
+        src_file.assert(predicates::path::missing());
+        dst_file.assert("1");
+    }
+
+    #[test]
+    fn copy_file() {
+        let src_file = NamedTempFile::new("a").unwrap();
+        let dst_file = NamedTempFile::new("b").unwrap();
+
+        src_file.write_str("1").unwrap();
+
+        assert_eq!(
+            transfer_path(src_file.path(), dst_file.path(), TransferMode::Copy)
+                .map_err(unpack_fse_error),
+            Ok(())
+        );
+
+        src_file.assert("1");
+        dst_file.assert("1");
+    }
+
+    #[test]
+    fn copy_overwrite_file() {
+        let src_file = NamedTempFile::new("a").unwrap();
+        let dst_file = NamedTempFile::new("b").unwrap();
+
+        src_file.write_str("1").unwrap();
+        dst_file.write_str("2").unwrap();
+
+        assert_eq!(
+            transfer_path(src_file.path(), dst_file.path(), TransferMode::Copy)
+                .map_err(unpack_fse_error),
+            Ok(())
+        );
+
+        src_file.assert("1");
+        dst_file.assert("1");
+    }
+
+    #[test]
+    fn rename_dir() {
+        let root_dir = TempDir::new().unwrap();
+
+        let src_dir = root_dir.child("a");
+        let src_file = src_dir.child("c");
+
+        let dst_dir = root_dir.child("b");
+        let dst_file = dst_dir.child("c");
+
+        src_dir.create_dir_all().unwrap();
+        src_file.write_str("1").unwrap();
+
+        assert_eq!(
+            transfer_path(src_dir.path(), dst_dir.path(), TransferMode::Move)
+                .map_err(unpack_fse_error),
+            Ok(())
+        );
+
+        src_dir.assert(predicates::path::missing());
+        src_file.assert(predicates::path::missing());
+
+        dst_dir.assert(predicates::path::is_dir());
+        dst_file.assert("1");
+    }
+
+    #[test]
+    fn move_overwrite_dir() {
+        let root_dir = TempDir::new().unwrap();
+
+        let src_dir = root_dir.child("a");
+        let src_file = src_dir.child("c");
+
+        let dst_dir = root_dir.child("b");
+        let dst_file = dst_dir.child("c");
+
+        src_dir.create_dir_all().unwrap();
+        src_file.write_str("1").unwrap();
+
+        dst_dir.create_dir_all().unwrap();
+        dst_file.write_str("2").unwrap();
+
+        assert_eq!(
+            transfer_path(src_dir.path(), dst_dir.path(), TransferMode::Move)
+                .map_err(unpack_fse_error),
+            Ok(())
+        );
+
+        src_dir.assert(predicates::path::missing());
+        src_file.assert(predicates::path::missing());
+
+        dst_dir.assert(predicates::path::is_dir());
+        dst_file.assert("1");
+    }
+
+    #[test]
+    fn copy_dir() {
+        let root_dir = TempDir::new().unwrap();
+
+        let src_dir = root_dir.child("a");
+        let src_file = src_dir.child("c");
+
+        let dst_dir = root_dir.child("b");
+        let dst_file = dst_dir.child("c");
+
+        src_dir.create_dir_all().unwrap();
+        src_file.write_str("1").unwrap();
+
+        assert_eq!(
+            transfer_path(src_dir.path(), dst_dir.path(), TransferMode::Copy)
+                .map_err(unpack_fse_error),
+            Ok(())
+        );
+
+        src_dir.assert(predicates::path::is_dir());
+        src_file.assert("1");
+
+        dst_dir.assert(predicates::path::is_dir());
+        dst_file.assert("1");
+    }
+
+    #[test]
+    fn copy_overwrite_dir() {
+        let root_dir = TempDir::new().unwrap();
+
+        let src_dir = root_dir.child("a");
+        let src_file = src_dir.child("c");
+
+        let dst_dir = root_dir.child("b");
+        let dst_file = dst_dir.child("c");
+
+        src_dir.create_dir_all().unwrap();
+        src_file.write_str("1").unwrap();
+
+        dst_dir.create_dir_all().unwrap();
+        dst_file.write_str("2").unwrap();
+
+        assert_eq!(
+            transfer_path(src_dir.path(), dst_dir.path(), TransferMode::Copy)
+                .map_err(unpack_fse_error),
+            Ok(())
+        );
+
+        src_dir.assert(predicates::path::is_dir());
+        src_file.assert("1");
+
+        dst_dir.assert(predicates::path::is_dir());
+        dst_file.assert("1");
     }
 }
