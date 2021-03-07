21e05ff0897273fe8f04dfc302df25f2358a499b
Jan Pikl <pikl.jan@gmail.com>
2021-03-07 18:37:13 +0100

    Refactor escape mod tests

--- src/bin/rew/pattern/escape.rs
+++ src/bin/rew/pattern/escape.rs
@@ -1,19 +1,6 @@
 use std::ascii::escape_default;
 use std::fmt::Write;
 
-fn append_escaped_char(string: &mut String, char: char) {
-    if char.is_ascii() {
-        if char == '\0' {
-            string.push_str("\\0"); // We do not want the default '\x00' output
-        } else {
-            write!(string, "{}", escape_default(char as u8))
-                .expect("Failed to append escaped char to string");
-        }
-    } else {
-        string.push(char);
-    }
-}
-
 pub fn escape_char(char: char) -> String {
     let mut result = String::new();
     append_escaped_char(&mut result, char);
@@ -28,29 +15,38 @@ pub fn escape_str(str: &str) -> String {
     result
 }
 
+fn append_escaped_char(string: &mut String, char: char) {
+    if char.is_ascii() {
+        if char == '\0' {
+            string.push_str("\\0"); // We do not want the default '\x00' output
+        } else {
+            write!(string, "{}", escape_default(char as u8))
+                .expect("Failed to append escaped char to string");
+        }
+    } else {
+        string.push(char);
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn escape_char() {
-        use super::*;
+    use super::*;
+    use test_case::test_case;
 
-        assert_eq!(escape_char('a'), String::from("a"));
-        assert_eq!(escape_char('á'), String::from("á"));
-        assert_eq!(escape_char('\0'), String::from("\\0"));
-        assert_eq!(escape_char('\x01'), String::from("\\x01"));
-        assert_eq!(escape_char('\n'), String::from("\\n"));
-        assert_eq!(escape_char('\r'), String::from("\\r"));
-        assert_eq!(escape_char('\t'), String::from("\\t"));
+    #[test_case('a' => "a"; "ascii")]
+    #[test_case('á' => "á"; "non-ascii")]
+    #[test_case('\0' => "\\0"; "null")]
+    #[test_case('\x01' => "\\x01"; "0x01")]
+    #[test_case('\n' => "\\n"; "line feed")]
+    #[test_case('\r' => "\\r"; "carriage return")]
+    #[test_case('\t' => "\\t"; "horizontal tab")]
+    fn escape_char(char: char) -> String {
+        super::escape_char(char)
     }
 
-    #[test]
-    fn escape_str() {
-        use super::*;
-
-        assert_eq!(escape_str("abc123"), String::from("abc123"));
-        assert_eq!(
-            escape_str("abc\0\0x01\n\r\táčď"),
-            String::from("abc\\0\\0x01\\n\\r\\táčď")
-        );
+    #[test_case("abc123" => "abc123"; "no escaping")]
+    #[test_case("abc\0\0x01\n\r\táčď" => "abc\\0\\0x01\\n\\r\\táčď"; "with escaping")]
+    fn escape_str(string: &str) -> String {
+        super::escape_str(string)
     }
 }
