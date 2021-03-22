8618608d2ad7347d00664c1a7b267ecb995ad892
Jan Pikl <pikl.jan@gmail.com>
2021-03-22 11:06:02 +0100

    Refactor counter mod tests

--- src/bin/rew/counter.rs
+++ src/bin/rew/counter.rs
@@ -161,13 +161,18 @@ mod tests {
             );
         }
 
-        #[test_case(0, 1, &[0, 1, 2]; "start zero increment one")]
-        #[test_case(1, 10, &[1, 11, 21]; "start one increment ten")]
-        fn next(init: u32, step: u32, sequence: &[u32]) {
+        #[test_case(0, 1, 0, 0; "0:1 iteration 1")]
+        #[test_case(0, 1, 1, 1; "0:1 iteration 2")]
+        #[test_case(0, 1, 2, 2; "0:1 iteration 3")]
+        #[test_case(1, 10, 0, 1; "1:10 iteration 1")]
+        #[test_case(1, 10, 1, 11; "1:10 iteration 2")]
+        #[test_case(1, 10, 2, 21; "1:10 iteration 3")]
+        fn next(init: u32, step: u32, index: usize, result: u32) {
             let mut counter = GlobalGenerator::new(init, step);
-            for (index, value) in sequence.iter().enumerate() {
-                assert_eq!(counter.next(), *value, "index {}", index);
+            for _ in 0..index {
+                counter.next();
             }
+            assert_eq!(counter.next(), result);
         }
     }
 
@@ -183,48 +188,34 @@ mod tests {
             );
         }
 
-        #[test_case(
-            0, 1, &[
-                ("a/b/c", 0),
-                ("a/b/c", 1),
-                ("a/b/c", 2),
-                ("a/b", 0),
-                ("a", 0),
-                ("a/b", 1),
-                ("a/b", 2),
-                ("a", 1),
-                ("a", 2),
-            ];
-            "start zero increment one"
-        )]
-        #[test_case(
-            1, 10, &[
-                ("a/b/c", 1),
-                ("a/b/c", 11),
-                ("a/b/c", 21),
-                ("a/b", 1),
-                ("a", 1),
-                ("a/b", 11),
-                ("a/b", 21),
-                ("a", 11),
-                ("a", 21),
-            ];
-            "start one increment ten"
-        )]
-        #[test_case(
-            0, 1, &[
-                ("a/b/c", 0),
-                ("./a/b/c", 1),
-                ("a", 0),
-                ("a/b/..", 1),
-            ];
-            "normalize dirs"
-        )]
-        fn next(init: u32, step: u32, sequence: &[(&str, u32)]) {
+        #[test_case(0, 1, &[], "a/b/c", 0; "0:1 iteration 1")]
+        #[test_case(0, 1, &["a/b/c"], "a/b/c", 1; "0:1 iteration 2")]
+        #[test_case(0, 1, &["a/b/c", "a/b/c"], "a/b/c", 2; "0:1 iteration 3")]
+        #[test_case(0, 1, &["a/b/c", "a/b/c", "a/b/c"], "a/b", 0; "0:1 iteration 4")]
+        #[test_case(0, 1, &["a/b/c", "a/b/c", "a/b/c", "a/b"], "a", 0; "0:1 iteration 5")]
+        #[test_case(0, 1, &["a/b/c", "a/b/c", "a/b/c", "a/b", "a"], "a/b", 1; "0:1 iteration 6")]
+        #[test_case(0, 1, &["a/b/c", "a/b/c", "a/b/c", "a/b", "a", "a/b"], "a/b", 2; "0:1 iteration 7")]
+        #[test_case(0, 1, &["a/b/c", "a/b/c", "a/b/c", "a/b", "a", "a/b", "a/b"], "a", 1; "0:1 iteration 8")]
+        #[test_case(0, 1, &["a/b/c", "a/b/c", "a/b/c", "a/b", "a", "a/b", "a/b", "a"], "a", 2; "0:1 iteration 9")]
+        #[test_case(1, 10, &[], "a/b/c", 1; "1:10 iteration 1")]
+        #[test_case(1, 10, &["a/b/c"], "a/b/c", 11; "1:10 iteration 2")]
+        #[test_case(1, 10, &["a/b/c", "a/b/c"], "a/b/c", 21; "1:10 iteration 3")]
+        #[test_case(1, 10, &["a/b/c", "a/b/c", "a/b/c"], "a/b", 1; "1:10 iteration 4")]
+        #[test_case(1, 10, &["a/b/c", "a/b/c", "a/b/c", "a/b"], "a", 1; "1:10 iteration 5")]
+        #[test_case(1, 10, &["a/b/c", "a/b/c", "a/b/c", "a/b", "a"], "a/b", 11; "1:10 iteration 6")]
+        #[test_case(1, 10, &["a/b/c", "a/b/c", "a/b/c", "a/b", "a", "a/b"], "a/b", 21; "1:10 iteration 7")]
+        #[test_case(1, 10, &["a/b/c", "a/b/c", "a/b/c", "a/b", "a", "a/b", "a/b"], "a", 11; "1:10 iteration 8")]
+        #[test_case(1, 10, &["a/b/c", "a/b/c", "a/b/c", "a/b", "a", "a/b", "a/b", "a"], "a", 21; "1:10 iteration 9")]
+        #[test_case(0, 1, &[], "a/b/c", 0; "normalize dirs 1")]
+        #[test_case(0, 1, &["a/b/c"], "./a/b/c", 1; "normalize dirs 2")]
+        #[test_case(0, 1, &["a/b/c", "./a/b/c"], "a", 0; "normalize dirs 3")]
+        #[test_case(0, 1, &["a/b/c", "./a/b/c", "a"], "a/b/..", 1; "normalize dirs 4")]
+        fn next(init: u32, step: u32, prev_paths: &[&str], next_path: &str, result: u32) {
             let mut counter = LocalGenerator::new(init, step);
-            for (index, (path, value)) in sequence.iter().enumerate() {
-                assert_eq!(counter.next(path), *value, "index {}", index);
+            for prev_path in prev_paths {
+                counter.next(prev_path);
             }
+            assert_eq!(counter.next(next_path), result);
         }
     }
 }
