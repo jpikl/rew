ad1428e98ae96b873fa38664bcafc36e61f10dac
Jan Pikl <pikl.jan@gmail.com>
2024-01-17 01:30:10 +0100

    Refactor PoC

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use anyhow::Error;
 use anyhow::Result;
-use bstr::BStr;
-use bstr::BString;
 use bstr::ByteSlice;
 use bstr::ByteVec;
 use clap::command;
@@ -11,12 +9,16 @@ use clap::Command;
 use clap::FromArgMatches;
 use clap::ValueEnum;
 use linereader::LineReader;
+use memchr::memchr;
+use memchr::memrchr;
 use owo_colors::OwoColorize;
 use std::fmt::Display;
 use std::fmt::Formatter;
+use std::io;
 use std::io::BufReader;
 use std::io::BufWriter;
 use std::io::IsTerminal;
+use std::io::Read;
 use std::io::StdinLock;
 use std::io::StdoutLock;
 use std::io::Write;
@@ -27,10 +29,27 @@ fn main() {
     handle_error(run().or_else(ignore_broken_pipe));
 }
 
+fn handle_error(result: Result<()>) {
+    if let Err(error) = result {
+        report_error(error, &mut anstream::stderr()).expect("Failed to write error to stderr!");
+        std::process::exit(1);
+    }
+}
+
+fn report_error(error: Error, stderr: &mut impl Write) -> io::Result<()> {
+    writeln!(stderr, "{}: {}", "error".red().bold(), error)?;
+
+    for cause in error.chain().skip(1) {
+        writeln!(stderr, "{}: {}", "cause".red(), cause)?;
+    }
+
+    Ok(())
+}
+
 fn ignore_broken_pipe(error: Error) -> Result<()> {
     for cause in error.chain() {
-        if let Some(io_error) = cause.downcast_ref::<std::io::Error>() {
-            if io_error.kind() == std::io::ErrorKind::BrokenPipe {
+        if let Some(io_error) = cause.downcast_ref::<io::Error>() {
+            if io_error.kind() == io::ErrorKind::BrokenPipe {
                 return Ok(());
             }
         }
@@ -53,35 +72,11 @@ fn run() -> Result<()> {
     unreachable!("clap should handle missing or invalid command");
 }
 
-pub struct CommandMeta {
-    name: &'static str,
-    build: fn() -> Command,
-    run: fn(&ArgMatches) -> Result<()>,
+fn get_commands() -> Vec<&'static CommandMeta> {
+    vec![&ASCII, &FIRST, &NORM, &TRIM]
 }
 
-const ASCII: CommandMeta = command_meta! {
-    name: "ascii",
-    args: AsciiArgs,
-    run: ascii,
-};
-
-const TRIM: CommandMeta = command_meta! {
-    name: "trim",
-    args: TrimArgs,
-    run: trim,
-};
-
-const NORM: CommandMeta = command_meta! {
-    name: "norm",
-    args: NormArgs,
-    run: norm,
-};
-
-pub fn get_commands() -> Vec<&'static CommandMeta> {
-    vec![&ASCII, &NORM, &TRIM]
-}
-
-pub fn build_app(commands: &Vec<&'static CommandMeta>) -> Command {
+fn build_app(commands: &Vec<&'static CommandMeta>) -> Command {
     let mut app = command!("rew").subcommand_required(true);
 
     for command in commands {
@@ -91,49 +86,12 @@ pub fn build_app(commands: &Vec<&'static CommandMeta>) -> Command {
     GlobalArgs::augment_args(app.next_help_heading("Global options"))
 }
 
-pub fn handle_error(result: Result<()>) {
-    if let Err(error) = result {
-        report_error(error, &mut anstream::stderr()).expect("Failed to write error to stderr!");
-        std::process::exit(1);
-    }
+struct CommandMeta {
+    name: &'static str,
+    build: fn() -> Command,
+    run: fn(&ArgMatches) -> Result<()>,
 }
 
-fn report_error(error: Error, stderr: &mut impl Write) -> std::io::Result<()> {
-    writeln!(stderr, "{}: {}", "error".red().bold(), error)?;
-
-    for cause in error.chain().skip(1) {
-        writeln!(stderr, "{}: {}", "cause".red(), cause)?;
-    }
-
-    Ok(())
-}
-
-#[derive(Clone, ValueEnum, Debug, PartialEq, Eq)]
-enum Buffering {
-    Line,
-    Block,
-}
-
-impl Display for Buffering {
-    fn fmt(&self, fmt: &mut Formatter<'_>) -> std::fmt::Result {
-        match &self {
-            Self::Line => write!(fmt, "line"),
-            Self::Block => write!(fmt, "block"),
-        }
-    }
-}
-
-impl Default for Buffering {
-    fn default() -> Self {
-        if std::io::stdout().is_terminal() {
-            Self::Line
-        } else {
-            Self::Block
-        }
-    }
-}
-
-#[macro_export]
 macro_rules! command_meta {
     (name: $name:literal, args: $args:ident, run: $run:ident,) => {
         CommandMeta {
@@ -156,8 +114,10 @@ struct GlobalArgs {
 
     /// Output buffering.
     ///
-    /// - `line` will flush output buffer after each line (default in interactive mode).
-    /// - `block` will flush output buffer when full (default in batch mode).
+    /// - `line` emits output after processing each line.
+    /// - `full` emits output once the output buffer is full (ensuring max possible throughput).
+    ///
+    /// Defaults to `line` when the output is TTY, otherwise is `full`.
     #[arg(
         global = true,
         long,
@@ -168,188 +128,162 @@ struct GlobalArgs {
     )]
     buff: Buffering,
 
-    /// Maximum size of an input line.
+    /// Maximum size of an input line (in bytes).
     ///
-    /// Attempt to process a bigger input line will result in an error and immediate termination.
-    /// Accepts values like `1024`, `1K`, `2MiB`, etc.
-    /// Using `0` as a value will disable the limit.
+    /// Attempt to process a longer input line will abort the execution with error.
     #[arg(
         global = true,
         long,
         name = "BYTES",
         env = "REW_MAX_LINE",
-        default_value_t = DEFAULT_CAPACITY,
+        default_value_t = OPTIMAL_IO_BUF_SIZE,
         verbatim_doc_comment,
     )]
-    max_line: u64,
+    max_line: usize,
 }
+
+const NORM: CommandMeta = command_meta! {
+    name: "norm",
+    args: NormArgs,
+    run: norm,
+};
+
+/// Normalize line separators to LF
+#[derive(Args)]
+struct NormArgs;
+
+fn norm(global_args: GlobalArgs, _args: NormArgs) -> Result<()> {
+    let mut reader = Reader::from(&global_args);
+    let mut writer = Writer::from(&global_args);
+
+    reader.for_each_line(|line| {
+        writer.write_line(line)?;
+        Ok(Processing::Continue)
+    })
+}
+
+const TRIM: CommandMeta = command_meta! {
+    name: "trim",
+    args: TrimArgs,
+    run: trim,
+};
 
 /// Trim whitespaces from each line.
 ///
 /// By default, both the beginning and the end are trimmed.
 #[derive(Args)]
 struct TrimArgs {
-    /// Trim the beginning
+    /// Trim the beginning.
     #[arg(short, long)]
     start: bool,
 
-    /// Trim the end
+    /// Trim the end.
     #[arg(short, long)]
     end: bool,
 }
 
 fn trim(global_args: GlobalArgs, args: TrimArgs) -> Result<()> {
-    let mut writer = RecordWriter::from(&global_args);
+    let mut reader = Reader::from(&global_args);
+    let mut writer = Writer::from(&global_args);
 
-    for_each_record(&global_args, |record| {
+    reader.for_each_line(|line| {
         let result = match (args.start, args.end) {
-            (true, true) | (false, false) => record.trim(),
-            (true, false) => record.trim_start(),
-            (false, true) => record.trim_end(),
+            (true, true) | (false, false) => line.trim(),
+            (true, false) => line.trim_start(),
+            (false, true) => line.trim_end(),
         };
-
-        writer.write(result)?;
-        Ok(true)
+        writer.write_line(result)?;
+        Ok(Processing::Continue)
     })
 }
+
+const ASCII: CommandMeta = command_meta! {
+    name: "ascii",
+    args: AsciiArgs,
+    run: ascii,
+};
 
 /// Convert characters to ASCII.
 #[derive(Args)]
-pub struct AsciiArgs {
+struct AsciiArgs {
     /// Delete non-ASCII characters instead of converting them.
     #[arg(short, long)]
-    pub delete: bool,
+    delete: bool,
 }
 
 fn ascii(global_args: GlobalArgs, args: AsciiArgs) -> Result<()> {
-    let mut writer = RecordWriter::from(&global_args);
-    let mut result = create_buffer(global_args.max_line as usize);
+    let mut reader = Reader::from(&global_args);
+    let mut writer = Writer::from(&global_args);
+    let mut buffer = Vec::with_capacity(OPTIMAL_IO_BUF_SIZE);
 
-    for_each_record(&global_args, |record| {
-        if record.is_ascii() {
-            writer.write(record)?;
+    reader.for_each_block(|block| {
+        // Copying chars to buffer is faster then directly writing them to output
+        if args.delete {
+            block
+                .chars()
+                .filter(|char| char.is_ascii())
+                .for_each(|char| buffer.push(char as u8));
         } else {
-            match args.delete {
-                true => record
-                    .chars()
-                    .filter(char::is_ascii)
-                    .for_each(|char| result.push_char(char)),
-                false => record
-                    .chars()
-                    .map(unidecode_char)
-                    .for_each(|char| result.push_str(char)),
-            }
-            writer.write(&result)?;
-            result.clear();
+            block
+                .chars()
+                .map(unidecode_char)
+                .for_each(|str| buffer.push_str(str));
         }
-        Ok(true)
+        writer.write_block(&buffer)?;
+        buffer.clear();
+        Ok(Processing::Continue)
     })
 }
 
-/// Norm
+const FIRST: CommandMeta = command_meta! {
+    name: "first",
+    args: FirstArgs,
+    run: first,
+};
+
+/// Output first N input line(s).
 #[derive(Args)]
-struct NormArgs {}
+struct FirstArgs {
+    /// Number of lines to print.
+    #[arg(default_value_t = 1)]
+    count: u128,
+}
 
-fn norm(global_args: GlobalArgs, _args: NormArgs) -> Result<()> {
-    let mut writer = RecordWriter::from(&global_args);
+fn first(global_args: GlobalArgs, args: FirstArgs) -> Result<()> {
+    let mut reader = Reader::from(&global_args);
+    let mut writer = Writer::from(&global_args);
+    let mut count = args.count;
 
-    for_each_record(&global_args, |record| {
-        writer.write(record)?;
-        Ok(true)
+    if count == 0 {
+        return Ok(());
+    }
+
+    reader.for_each_line(|line| {
+        writer.write_line(line)?;
+        count -= 1;
+
+        if count > 0 {
+            Ok(Processing::Continue)
+        } else {
+            Ok(Processing::Abort)
+        }
     })
 }
 
-const DEFAULT_CAPACITY: u64 = 1024 * 64;
-
-fn for_each_record<F: FnMut(&BStr) -> Result<bool>>(
-    args: &GlobalArgs,
-    mut action: F,
-) -> Result<()> {
-    let mut result = Ok(());
-    let capacity = args.max_line as usize;
-    if args.null {
-        LineReader::with_delimiter_and_capacity(b'\0', capacity, open_stdin()).for_each(|record| {
-            let trimmed_record = trim_null(record);
-            if trimmed_record.len() == capacity {
-                result = Err(MaxLineError(args.max_line).into());
-                return Ok(false);
-            }
-            match action(trimmed_record.as_bstr()) {
-                Ok(true) => Ok(true),
-                Ok(false) => Ok(false),
-                Err(err) => {
-                    result = Err(err);
-                    Ok(false)
-                }
-            }
-        })
-    } else {
-        LineReader::with_delimiter_and_capacity(b'\n', capacity, open_stdin()).for_each(|record| {
-            let trimmed_record = trim_newline(record);
-            if trimmed_record.len() == capacity {
-                result = Err(MaxLineError(args.max_line).into());
-                return Ok(false);
-            }
-            match action(trimmed_record.as_bstr()) {
-                Ok(true) => Ok(true),
-                Ok(false) => Ok(false),
-                Err(err) => {
-                    result = Err(err);
-                    Ok(false)
-                }
-            }
-        })
-    }?;
-    result
-}
-
-fn trim_newline(mut line: &[u8]) -> &[u8] {
-    if line.last_byte() == Some(b'\n') {
-        line = &line[..line.len() - 1];
-        if line.last_byte() == Some(b'\r') {
-            line = &line[..line.len() - 1];
-        }
-    }
-    line
-}
-
-fn trim_null(mut record: &[u8]) -> &[u8] {
-    if record.last_byte() == Some(b'\0') {
-        record = &record[..record.len() - 1];
-    }
-    record
-}
-
-fn create_buffer(capacity: usize) -> BString {
-    Vec::with_capacity(capacity).into()
-}
-
-fn open_stdin() -> BufReader<StdinLock<'static>> {
-    BufReader::new(std::io::stdin().lock())
-}
-
-fn open_stdout() -> BufWriter<StdoutLock<'static>> {
-    BufWriter::new(std::io::stdout().lock())
-}
+// Optimal value for max IO throughput, according to https://www.evanjones.ca/read-write-buffer-size.html
+// Also confirmed by some custom benchmarks.
+// Also used internally by the `linereader` library.
+const OPTIMAL_IO_BUF_SIZE: usize = 32 * 1024;
 
 #[derive(Debug, Error, PartialEq)]
-#[error("cannot process input line bigger than '{}'", 123)]
-struct MaxLineError(u64);
+#[error("cannot process input line bigger than '{}' bytes", .0)]
+struct MaxLineError(usize);
 
 #[derive(Clone, Default, Debug, PartialEq, Eq)]
-pub enum Separator {
+enum Separator {
     #[default]
     Newline,
     Null,
-}
-
-impl Separator {
-    pub fn as_byte(&self) -> u8 {
-        match self {
-            Separator::Newline => b'\n',
-            Separator::Null => b'\0',
-        }
-    }
 }
 
 impl From<&GlobalArgs> for Separator {
@@ -361,38 +295,194 @@ impl From<&GlobalArgs> for Separator {
     }
 }
 
-pub struct RecordWriter<W> {
-    writer: W,
+impl Separator {
+    fn as_byte(&self) -> u8 {
+        match self {
+            Self::Newline => b'\n',
+            Self::Null => b'\0',
+        }
+    }
+
+    fn trim_end<'a>(&self, mut line: &'a [u8]) -> &'a [u8] {
+        match self {
+            Self::Newline => {
+                if line.last_byte() == Some(b'\n') {
+                    line = &line[..line.len() - 1];
+                    if line.last_byte() == Some(b'\r') {
+                        line = &line[..line.len() - 1];
+                    }
+                }
+            }
+            Self::Null => {
+                if line.last_byte() == Some(b'\0') {
+                    line = &line[..line.len() - 1];
+                }
+            }
+        }
+        line
+    }
+}
+
+#[derive(Clone, ValueEnum, Debug, PartialEq, Eq)]
+enum Buffering {
+    Line,
+    Full,
+}
+
+impl Display for Buffering {
+    fn fmt(&self, fmt: &mut Formatter<'_>) -> std::fmt::Result {
+        match &self {
+            Self::Line => write!(fmt, "line"),
+            Self::Full => write!(fmt, "full"),
+        }
+    }
+}
+
+impl Default for Buffering {
+    fn default() -> Self {
+        if io::stdout().is_terminal() {
+            Self::Line
+        } else {
+            Self::Full
+        }
+    }
+}
+
+enum Processing {
+    Continue,
+    Abort,
+}
+
+struct Reader<R> {
+    inner: R,
+    separator: Separator,
+    max_line: usize,
+}
+
+impl From<&GlobalArgs> for Reader<StdinLock<'static>> {
+    fn from(args: &GlobalArgs) -> Self {
+        Self::new(io::stdin().lock(), Separator::from(args), args.max_line)
+    }
+}
+
+impl<R: Read> Reader<R> {
+    fn new(inner: R, separator: Separator, max_line: usize) -> Self {
+        Self {
+            inner,
+            separator,
+            max_line,
+        }
+    }
+
+    fn for_each_line<F: FnMut(&[u8]) -> Result<Processing>>(
+        &mut self,
+        mut action: F,
+    ) -> Result<()> {
+        let byte_separator = self.separator.as_byte();
+        let mut reader =
+            LineReader::with_delimiter_and_capacity(byte_separator, self.max_line, &mut self.inner);
+
+        while let Some(batch) = reader.next_batch() {
+            let mut batch = batch?;
+
+            while let Some(end) = memchr(byte_separator, batch) {
+                let (line, next_batch) = batch.split_at(end + 1);
+
+                if line.len() == self.max_line {
+                    return Err(MaxLineError(self.max_line).into());
+                }
+
+                match action(self.separator.trim_end(line)) {
+                    Ok(Processing::Continue) => {}
+                    Ok(Processing::Abort) => return Ok(()),
+                    Err(err) => return Err(err),
+                }
+
+                batch = next_batch;
+            }
+
+            if !batch.is_empty() {
+                match action(self.separator.trim_end(batch)) {
+                    Ok(Processing::Continue) => {}
+                    Ok(Processing::Abort) => return Ok(()),
+                    Err(err) => return Err(err),
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn for_each_block<F: FnMut(&[u8]) -> Result<Processing>>(
+        &mut self,
+        mut action: F,
+    ) -> Result<()> {
+        let mut reader = BufReader::new(&mut self.inner);
+        let mut buffer = vec![0; OPTIMAL_IO_BUF_SIZE];
+
+        loop {
+            let len = reader.read(&mut buffer)?;
+            if len == 0 {
+                break;
+            }
+            match action(&buffer[..len]) {
+                Ok(Processing::Continue) => {}
+                Ok(Processing::Abort) => return Ok(()),
+                Err(err) => return Err(err),
+            }
+        }
+
+        Ok(())
+    }
+}
+
+struct Writer<W> {
+    inner: W,
     separator: u8,
     buffering: Buffering,
 }
 
-impl From<&GlobalArgs> for RecordWriter<BufWriter<StdoutLock<'static>>> {
+impl From<&GlobalArgs> for Writer<BufWriter<StdoutLock<'static>>> {
     fn from(args: &GlobalArgs) -> Self {
-        Self::new(
-            open_stdout(),
-            Separator::from(args).as_byte(),
-            args.buff.clone(),
-        )
+        let inner = BufWriter::with_capacity(OPTIMAL_IO_BUF_SIZE, io::stdout().lock());
+        Self::new(inner, Separator::from(args), args.buff.clone())
     }
 }
 
-impl<W: Write> RecordWriter<W> {
-    fn new(writer: W, separator: u8, buffering: Buffering) -> Self {
+impl<W: Write> Writer<W> {
+    fn new(inner: W, separator: Separator, buffering: Buffering) -> Self {
         Self {
-            writer,
-            separator,
+            inner,
+            separator: separator.as_byte(),
             buffering,
         }
     }
 
-    fn write(&mut self, value: &[u8]) -> Result<()> {
-        self.writer.write_all(value)?;
-        self.writer.write_all(&[self.separator])?;
+    fn write_line(&mut self, line: &[u8]) -> Result<()> {
+        self.inner.write_all(line)?;
+        self.inner.write_all(&[self.separator])?;
 
         match self.buffering {
-            Buffering::Line => self.writer.flush().map_err(Into::into),
-            Buffering::Block => Ok(()),
+            Buffering::Line => self.inner.flush().map_err(Into::into),
+            Buffering::Full => Ok(()),
         }
+    }
+
+    fn write_block(&mut self, block: &[u8]) -> Result<()> {
+        match self.buffering {
+            Buffering::Line => {
+                // We do not care much about the performance in this mode
+                if let Some(pos) = memrchr(self.separator, block) {
+                    let (before, after) = block.split_at(pos + 1);
+                    self.inner.write_all(before)?;
+                    self.inner.flush()?;
+                    self.inner.write_all(after)?;
+                } else {
+                    self.inner.write_all(block)?;
+                }
+            }
+            Buffering::Full => self.inner.write_all(block)?,
+        }
+        Ok(())
     }
 }
