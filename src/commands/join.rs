3cb6b1bb45de3d07d464eee461ff3ff1c32f10c8
Jan Pikl <pikl.jan@gmail.com>
2024-02-02 01:16:42 +0100

    Make join implementation more readable

--- src/commands/join.rs
+++ src/commands/join.rs
@@ -27,29 +27,34 @@ fn run(context: &Context, args: &Args) -> Result<()> {
     let mut reader = context.block_reader();
     let mut writer = context.writer();
 
-    let trim_sparator = context.separator().trim_fn();
-    let input_separator = context.separator().as_byte();
     let output_separator = args.separator.as_bytes();
+    let input_separator = context.separator().as_byte();
+    let trim_input_sparator = context.separator().trim_fn();
 
-    let mut separate_next_block = false;
+    let mut start_next_item_separated = false;
 
-    while let Some(mut block) = reader.read_block()? {
-        while let Some(pos) = memchr(input_separator, block) {
-            if separate_next_block {
+    while let Some(block) = reader.read_block()? {
+        let mut remainder: &[u8] = block;
+
+        while let Some(pos) = memchr(input_separator, remainder) {
+            if start_next_item_separated {
                 writer.write_block(output_separator)?;
-            } else {
-                separate_next_block = true;
             }
-            writer.write_block(trim_sparator(&block[..=pos]))?;
-            block = &mut block[(pos + 1)..];
+
+            writer.write_block(trim_input_sparator(&remainder[..=pos]))?;
+            remainder = &remainder[(pos + 1)..];
+
+            // Write the trimmed separator once we know there is more data
+            start_next_item_separated = true;
         }
 
-        if !block.is_empty() {
-            if separate_next_block {
+        if !remainder.is_empty() {
+            if start_next_item_separated {
                 writer.write_block(output_separator)?;
-                separate_next_block = false;
             }
-            writer.write_block(block)?;
+
+            writer.write_block(remainder)?;
+            start_next_item_separated = false;
         }
     }
 
